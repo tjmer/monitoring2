35246f4670043c945c407d7cbe53d75a47970c98
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 12:57:32 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.PORT || 4545
+app.listen(port, ()=>console.log(`Up on ${port}`))
